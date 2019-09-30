4fb0d0bf4e226828eb318919f67ba32666be9850
kiam001 <blablatupfen@gmail.com>
2019-09-30 18:51:52 +0200

    Typo

--- OtherRoms.sh
+++ OtherRoms.sh
@@ -7,7 +7,7 @@ read rom
 devicedir=device/motorola/cedric
 cd $devicedir
 mv aosp_cedric".mk" $rom"_"cedric".mk"
-newmk=$rom"_"cedic".mk"
+newmk=$rom"_"cedric".mk"
 sed -i -e 's/aosp/'$rom/g $newmk
 sed -i -e 's/aosp/'$rom/g vendorsetup.sh
 sed -i -e 's/aosp/'$rom/g AndroidProducts.mk
