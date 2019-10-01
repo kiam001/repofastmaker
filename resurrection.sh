6dff5e716afdebd6b39d737fed7faeae76807daf
Maik Stratemeyer <blablatupfen@gmail.com>
2019-10-01 13:57:49 +0200

    add resurrection

---
+++ resurrection.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/kiam001/android_device_motorola_cedric -b resurrection device/motorola/cedric
+git clone https://github.com/kiam001/android_vendor_motorola_cedric vendor/motorola/cedric
+git clone https://github.com/kiam001/android_kernel_motorola_msm8937 kernel/motorola/msm8937
+git clone https://github.com/kiam001/packages_resources_MotoActions packages/resources/MotoActions
+git clone https://github.com/ArrowOS/android_system_qcom system/qcom
