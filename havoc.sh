6154c0b32d1c4d37b6ab9098271d6b7f108f9d6b
Maik Stratemeyer <blablatupfen@gmail.com>
2019-10-02 15:06:55 +0200

    added some

--- havoc.sh
+++ havoc.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/kiam001/android_device_motorola_cedric -b DerpFest device/motorola/cedric
+git clone https://github.com/kiam001/android_device_motorola_cedric -b havoc device/motorola/cedric
 git clone https://github.com/kiam001/android_vendor_motorola_cedric vendor/motorola/cedric
 git clone https://github.com/kiam001/android_kernel_motorola_msm8937 kernel/motorola/msm8937
 git clone https://github.com/kiam001/packages_resources_MotoActions packages/resources/MotoActions
