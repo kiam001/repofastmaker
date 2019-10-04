5a08fab3aff309f4d7fc185dc1d599e4fab90572
Maik Stratemeyer <blablatupfen@gmail.com>
2019-10-04 07:58:55 +0200

    fix screenshot

--- DerpFest.sh
+++ DerpFest.sh
@@ -5,6 +5,9 @@ git clone https://github.com/kiam001/packages_resources_MotoActions packages/res
 git clone https://github.com/kiam001/android_system_qcom system/qcom
 repo init -u git://github.com/DerpFest-OP5T/platform_manifest.git -b pie
 repo sync -c -j$( nproc --all ) --force-sync --no-clone-bundle --no-tags
+cd device/aosip/sepolicy
+git revert 486869a -n
+cd ../../../
 . build/envsetup.sh
 lunch aosip_cedric-userdebug
 mka kronic
