1d896957c22fcf3ca57d51f9335d2fe7e2d6e8fd
Maik Stratemeyer <blablatupfen@gmail.com>
2019-10-19 19:35:09 +0200

    Add support for ubuntu touch

---
+++ halium.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/ubports-on-cedric/android_device_motorola_cedric -b ubp-7.1  device/motorola/cedric
+git clone https://github.com/ubports-on-cedric/android_vendor_motorola_cedric -b ubp-7.1 vendor/motorola/cedric
+git clone https://github.com/ubports-on-cedric/android_kernel_motorola_msm8937 -b ubp-7.1 kernel/motorola/msm8937
+repo init -u https://github.com/Halium/android -b halium-7.1 --depth=1
+repo sync -c -j 16
+./halium/devices/setup cedric
+source build/envsetup.sh
+breakfast cedric
+echo done
