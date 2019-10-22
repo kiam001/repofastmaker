90f0d2c25589439160c764e9fb3e37a43b453512
kiam001 <blablatupfen@gmail.com>
2019-10-22 10:38:10 +0200

    Add Eva Kernel

--- crdroid.sh
+++ crdroid.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/kiam001/android_device_motorola_cedric -b crdroid device/motorola/cedric
 git clone https://github.com/kiam001/android_vendor_motorola_cedric vendor/motorola/cedric
-git clone https://github.com/kiam001/android_kernel_motorola_msm8937 kernel/motorola/msm8937
+git https://github.com/mvaisakh/eva_kernel/ -b eva-rel/eas+/3.18 kernel/motorola/msm8937
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings packages/resources/devicesettings
 git clone https://github.com/kiam001/android_system_qcom system/qcom
 repo init -u git://github.com/crdroidandroid/android.git -b 9.0
