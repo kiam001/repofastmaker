928c43352a00fe9ba9025ce09e4f85bf7df72880
Maik Stratemeyer <blablatupfen@gmail.com>
2019-10-22 10:27:56 +0200

    Add Eva kernel

--- MSM-Xtended.sh
+++ MSM-Xtended.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/kiam001/android_device_motorola_cedric -b MSM-Xtended device/motorola/cedric
 git clone https://github.com/kiam001/android_vendor_motorola_cedric vendor/motorola/cedric
-git clone https://github.com/kiam001/android_kernel_motorola_msm8937 kernel/motorola/msm8937
+git clone https://github.com/mvaisakh/eva_kernel/ -b eva-rel/eas+/3.18 kernel/motorola/msm8937
 git clone https://github.com/kiam001/packages_resources_MotoActions packages/resources/MotoActions
 repo init -u https://github.com/Project-Xtended/manifest.git -b xp
 repo sync -f -c -j8 --force-sync --no-clone-bundle --no-tags
--- resurrection.sh
+++ resurrection.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/kiam001/android_device_motorola_cedric -b resurrection device/motorola/cedric
 git clone https://github.com/kiam001/android_vendor_motorola_cedric vendor/motorola/cedric
-git clone https://github.com/kiam001/android_kernel_motorola_msm8937 kernel/motorola/msm8937
+git clone https://github.com/mvaisakh/eva_kernel/ -b eva-rel/eas+/3.18 kernel/motorola/msm8937
 git clone https://github.com/kiam001/packages_resources_MotoActions packages/resources/MotoActions
 git clone https://github.com/kiam001/android_system_qcom system/qcom
 repo init -u https://github.com/ResurrectionRemix/platform_manifest.git -b pie
