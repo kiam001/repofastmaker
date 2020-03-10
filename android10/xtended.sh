99756e102f3014844d0fbb719c382a3e1f782c0e
kiam001 <blablatupfen@gmail.com>
2020-03-10 15:04:04 +0100

    Get automatic CPU Numbers on build

--- android10/xtended.sh
+++ android10/xtended.sh
@@ -7,7 +7,7 @@ git clone https://github.com/kiam001/Xtended_.repo_local_manifests .repo/local_m
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 . build/envsetup.sh
 lunch xtended_cedric-userdebug
-make xtended -j4
+make xtended -j$(nproc --all)
 scp /home/maik/build/xtended/out/target/product/cedric/Xtended-cedric-UNOFFICIAL-* "kiam001@frs.sourceforge.net:/home/frs/project/kiam001-build-roms/Device\ Cedric\ \(Moto\ G5\)/android10"
 cd ../
 rm -rf xtended
