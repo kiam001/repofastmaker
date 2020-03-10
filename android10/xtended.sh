c5395b65d70dc6716e7cefedc1e9545219e1cc9c
kiam001 <maik@kiam001.tk>
2020-03-10 14:52:13 +0100

    bring repofastmaker back and begin with xtended

---
+++ android10/xtended.sh
@@ -0,0 +1,10 @@
+mkdir xtended
+cd xtended
+repo init -u https://github.com/Project-Xtended/manifest.git -b r31
+git clone https://github.com/kiam001/Xtended_.repo_local_manifests .repo/local_manifests
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+. build/envsetup.sh
+lunch xtended_cedric-userdebug
+make xtended -j4
+scp /home/maik/build/xtended/out/target/product/cedric/Xtended-cedric-UNOFFICIAL-* "kiam001@frs.sourceforge.net:/home/frs/project/kiam001-build-roms/Device\ Cedric\ \(Moto\ G5\)/android10"
+echo done
