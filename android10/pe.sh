49083f622c66d5f9fced9fd81a35ceb875b19b44
Maik Stratemeyer <maik@stratemeyet.tk>
2020-03-26 16:13:56 +0100

    Add Pe (Not Plus)  Support

---
+++ android10/pe.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+touch PROCESSRUNNING
+mkdir -p builds
+mkdir pe
+cd pe
+repo init -u https://github.com/PixelExperience/manifest -b ten
+git clone https://github.com/kiam001/PixelExperience_.repo_local_manifests .repo/local_manifests
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+. build/envsetup.sh
+lunch aosp_cedric-userdebug
+mka bacon -j$(nproc --all)
+scp out/target/product/cedric/PixelExperience* "kiam001@frs.sourceforge.net:/home/frs/project/kiam001-build-roms/Device\ Cedric\ \(Moto\ G5\)/android10"
+cp out/target/product/cedric/PixelExperience* ../builds
+cd ../
+rm -rf pe
+rm PROCESSRUNNING
+echo done
