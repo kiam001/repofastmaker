4df25b986f6a7a68965a10d320dc302ae4de9a96
Maik Stratemeyer <maik@stratemeyet.tk>
2020-04-03 22:50:28 +0200

    Add Support for Atom

---
+++ android10/atom.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+touch PROCESSRUNNING
+mkdir -p builds
+mkdir atom
+cd atom
+repo init -u https://github.com/AtomOrganization/manifest -b ten --depth=1
+git clone https://github.com/kiam001/Atom_.repo_local_manifests .repo/local_manifests
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+. build/envsetup.sh
+lunch aosp_cedric-userdebug
+mka bacon -j$(nproc --all)
+scp out/target/product/cedric/Atom* "kiam001@frs.sourceforge.net:/home/frs/project/kiam001-build-roms/Device\ Cedric\ \(Moto\ G5\)/android10"
+cp out/target/product/cedric/Atom* ../builds
+cd ../
+rm -rf atom
+rm PROCESSRUNNING
+echo done
