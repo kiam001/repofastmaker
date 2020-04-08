a57e4ab8773a085ed65e6dffba1c7413f082f0ae
kiam001 <blablatupfen@gmail.com>
2020-04-09 00:38:23 +0200

    fix automatic

--- android10/atom-violet.sh
+++ android10/atom-violet.sh
@@ -13,9 +13,9 @@ repo init -u https://github.com/AtomOrganization/manifest -b ten
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 git clone https://github.com/Descendant-Devices/android_device_xiaomi_violet device/xiaomi/violet
 cd device/xiaomi/violet
-git revert 7cf197aa8fdf79fd0820b6ae713e76f35ea2f139
-git revert 69a447ccfdb8a824fd0d37a4ebbfddf767bcca0d
-git revert 624eed9fccaad159d7d9418db49cff060af1bbd9
+git revert 7cf197aa8fdf79fd0820b6ae713e76f35ea2f139 --no-commit
+git revert 69a447ccfdb8a824fd0d37a4ebbfddf767bcca0d --no-commit
+git revert 624eed9fccaad159d7d9418db49cff060af1bbd9 --no-commit
 cd ../../..
 git clone --depth 1 https://github.com/tesla59/hydra_kernel kernel/xiaomi/sm6150
 git clone --depth 1 https://github.com/tesla59/vendor_xiaomi_violet vendor/xiaomi/violet
