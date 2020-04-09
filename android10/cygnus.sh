f54db8a96fd949071e76e35d7513167c8803017c
kiam001 <blablatupfen@gmail.com>
2020-04-09 14:59:51 +0200

    Setup cygnus finish

--- android10/cygnus.sh
+++ android10/cygnus.sh
@@ -9,12 +9,13 @@ touch PROCESSRUNNING
 mkdir -p builds
 mkdir cygnus
 cd cygnus
-repo init -u https://github.com/AtomOrganization/manifest -b ten
+repo init -u https://github.com/cygnus-rom/manifest.git -b caf-ten
 git clone https://github.com/kiam001/cygnus_.repo_local_manifests .repo/local_manifests
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle --prune --optimized-fetch
+repo forall -c git lfs pull
 . build/envsetup.sh
-lunch aosp_cedric-userdebug
-mka bacon -j$(nproc --all)
+lunch cygnus_cedric-userdebug
+mka cygnus
 scp out/target/product/cedric/Cygnus* "kiam001@frs.sourceforge.net:/home/frs/project/kiam001-build-roms/Device\ Cedric\ \(Moto\ G5\)/android10"
 cp out/target/product/cedric/Cygnus* ../builds
 cd ../
