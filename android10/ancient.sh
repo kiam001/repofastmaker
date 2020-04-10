b90885ec377a4f34e5f6e4965936b9b9bd710d2e
kiam001 <blablatupfen@gmail.com>
2020-04-10 15:36:46 +0200

    Add support for ancient (debug)

---
+++ android10/ancient.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mkdir ~/bin
+PATH=~/bin:$PATH
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+git config --global user.name "kiam001"
+git config --global user.email "blablatupfen@gmail.com"
+chmod a+x ~/bin/repo
+touch PROCESSRUNNING
+mkdir -p builds
+mkdir ancient
+cd ancient
+repo init -u https://github.com/Ancient-Lab/manifest -b ten
+git clone https://github.com/kiam001/Ancient_.repo_local_manifests .repo/local_manifests
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+. build/envsetup.sh
+lunch ancient_cedric-userdebug
+mka bacon -j$(nproc --all)
+scp out/target/product/cedric/Ancient* "kiam001@frs.sourceforge.net:/home/frs/project/kiam001-build-roms/Device\ Cedric\ \(Moto\ G5\)/android10"
+cp out/target/product/cedric/Ancient* ../builds
+cd ../
+#rm -rf ancient
+rm PROCESSRUNNING
+echo done
