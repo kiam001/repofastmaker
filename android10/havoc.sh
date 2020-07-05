3d3c5a52c0301c3b147399c768f0bfe80ecd47fb
kiam001 <blablatupfen@gmail.com>
2020-07-05 13:34:03 +0200

    Havoc add Gapps to normal

--- android10/havoc.sh
+++ android10/havoc.sh
@@ -13,8 +13,9 @@ repo init -u https://github.com/Havoc-OS/android_manifest.git -b ten --depth=1
 git clone https://github.com/kiam001/all10_.repo_local_manifests -b havoc .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 . build/envsetup.sh
-lunch havoc_cedric-userdebug
-mka bacon -j$(nproc --all)
+brunch cedric
+export WITH_GAPPS=true
+brunch cedric
 scp out/target/product/cedric/Havoc* "kiam001@frs.sourceforge.net:/home/frs/project/kiam001-build-roms/Device\ Cedric\ \(Moto\ G5\)/android10"
 cp out/target/product/cedric/Havoc* ../builds
 cd ../
